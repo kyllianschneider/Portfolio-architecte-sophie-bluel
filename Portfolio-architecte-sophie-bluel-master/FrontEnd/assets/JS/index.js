86db93e0fb724364817111750cf67def3dbd8f12
kidlysama <kylli444@gmail.com>
2023-03-05 15:55:05 +0100

    clearer index.js and removed jquery for the preview

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/JS/index.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/JS/index.js
@@ -1,23 +1,11 @@
-if (auth()) {
-    a = document.createElement("a");
-    a.href = "./index.html";
-    a.innerHTML = "logout";
-    a.addEventListener("click", function (e) {
-        deleteAllCookies();
-    });
-    gid("log_zone").appendChild(a);
-    gid("modif_projet").hidden = false;
-    gid("icon1").hidden = false;
-    gid("edit_mode").style.display = "flex";
-    gid("modif-button").style.display = "flex";
-    gid("projet").style.marginLeft = "0px";
-} else {
-    gid("log_zone").innerHTML = '<a href="./login.html">login</a>';
-}
+//-- set global variable ---
+const allWork = new Set();
+const objWork = new Set();
+const apptWork = new Set();
+const hotelWork = new Set();
 
-var cat = 0; // 0 = all /// catégorie actuelle
-var categories; // liste des catégories
-var portfolio; // liste des projets
+const gallery = document.querySelector("#gallery");
+const manager = document.querySelector("#manager");
 
 mypop = new popup("mypopup");
 mypop2 = new popup("mypopup2");
@@ -29,7 +17,7 @@ function nextpop() {
 
 function back() {
     mypop2.close();
-    document.getElementById("blah").removeAttribute("src");
+    document.getElementById("preview").removeAttribute("src");
     mypop.pop();
 }
 
@@ -45,20 +33,6 @@ function closepopup() {
 
 function openpopup() {
     mypop.pop();
-}
-
-function create_figure_dom(elem_portfolio) {
-    caption = document.createElement("figcaption");
-    caption.innerHTML = elem_portfolio.title;
-    image = document.createElement("img");
-    image.src = elem_portfolio.imageUrl;
-    image.crossorigin = "anonymous";
-    image.setAttribute("crossorigin", "anonymous");
-    obj = document.createElement("figure");
-    obj.setAttribute("cat_id", elem_portfolio.categoryId); // ajout de l'id de categorie comme atribu
-    obj.appendChild(image);
-    obj.appendChild(caption);
-    return obj;
 }
 
 function supprimer_proj(id_proj) {
@@ -78,88 +52,138 @@ function supprimer_proj(id_proj) {
     });
 }
 
-fetch(serv + "api/works", { mode: "cors" })
-    .then((r) => r.json())
-    .then((data) => {
-        portfolio = data;
-        //affichage du portfolio
-        // console.log(portfolio);
-        for (el in portfolio) {
-            gid("gallery").appendChild(create_figure_dom(portfolio[el]));
-            temp = create_figure_dom(portfolio[el]);
-            button = document.createElement("div");
-            button.style.backgroundImage =
-                "url('./assets/icons/trash-can-solid.svg')";
-            button.className = "supp";
-            button.setAttribute("proj_id", portfolio[el].id);
-            button.addEventListener("click", function (e) {
-                supprimer_proj(e.target.getAttribute("proj_id"));
-                e.target.parentNode.style = "display: none;";
-                e.stopPropagation(); // cache temporairement le div
-                e.preventDefault();
-            });
-            temp.appendChild(button);
-            gid("manager").appendChild(temp); // figcaption masqué en css
-        }
-    });
+// portfolio = data;
+//affichage du portfolio
+// console.log(portfolio);
+// for (work in works) {
+// temp = displayImages(works);
+// button = document.createElement("div");
+// button.style.backgroundImage =
+//     "url('./assets/icons/trash-can-solid.svg')";
+// button.className = "supp";
+// button.setAttribute("proj_id", works.id);
+// button.addEventListener("click", function (e) {
+//     supprimer_proj(e.target.getAttribute("proj_id"));
+//     e.target.parentNode.style = "display: none;";
+//     e.stopPropagation(); // cache temporairement le div
+//     e.preventDefault();
+// });
+// temp.appendChild(button);
+// gid("manager").appendChild(temp); // figcaption masqué en css
+// }
 
-// ajax de la liste des catégories
-fetch(serv + "api/categories", { mode: "cors" })
-    .then((r) => r.json())
-    .then((data) => {
-        categories = data;
-
-        categories[-1] = {};
-        categories[-1].id = 0;
-        categories[-1].name = "Tous";
-
-        //création des divs pour les filtres
-        for (i = -1; i < categories.length; i++) {
-            objdiv = document.createElement("div");
-            objdiv.innerHTML = categories[i].name;
-            objdiv.id = parseInt(i) + 1;
-            objdiv.addEventListener("click", function (e) {
-                cat = e.target.id;
-                display_cat();
-            });
-            gid("categories").appendChild(objdiv);
-        }
-    });
-
-function display_cat() {
-    gallery = gid("gallery");
-    //boucle sur chaque element du portfolio
-    for (var z = 0; z < gallery.childNodes.length; z++) {
-        if (gallery.childNodes[z].nodeType == 1) {
-            // type 1 seulement ELEMENT_NODE
-            gallery.childNodes[z].style = "display: none;";
-            if (
-                gallery.childNodes[z].getAttribute("cat_id") == cat ||
-                cat == 0
-            ) {
-                gallery.childNodes[z].style = "display: block;";
-            }
-        }
+//--- Admin section ----
+function setAdminElement() {
+    if (auth()) {
+        a = document.createElement("a");
+        a.href = "./index.html";
+        a.innerHTML = "logout";
+        a.addEventListener("click", function (e) {
+            deleteAllCookies();
+        });
+        gid("log_zone").appendChild(a);
+        gid("modif_projet").hidden = false;
+        gid("icon1").hidden = false;
+        gid("edit_mode").style.display = "flex";
+        gid("modif-button").style.display = "flex";
+        gid("projet").style.marginLeft = "0px";
+    } else {
+        gid("log_zone").innerHTML = '<a href="./login.html">login</a>';
     }
 }
 
+//---- fetch request -----
+async function getAllWorks() {
+    const response = await fetch("http://localhost:5678/api/works");
+    if (response.ok) {
+        return response.json();
+    } else {
+        console.log(response);
+    }
+}
+
+//--- triage des works ---//
+async function initiateWorksSet() {
+    const works = await getAllWorks();
+    works.forEach((work) => {
+        if (work.categoryId == 1) {
+            objWork.add(work);
+        }
+        if (work.categoryId == 2) {
+            apptWork.add(work);
+        }
+        if (work.categoryId == 3) {
+            hotelWork.add(work);
+        }
+        allWork.add(work);
+    });
+    displayImages(allWork);
+}
+
+//---- Affichage Image -----
+async function displayImages(works) {
+    const imgFragment = document.createDocumentFragment();
+    for (const work of works) {
+        const figure = document.createElement("figure");
+        figure.dataset.cat = work.categoryId;
+        figure.innerHTML = `<img src="${work.imageUrl}" alt="${work.title}" crossorigin="anonymous">
+            <figcaption>${work.title}</figcaption>`;
+        imgFragment.appendChild(figure);
+    }
+    gallery.appendChild(imgFragment);
+}
+
+//--- vider la gallery ------
+function clearGallery() {
+    gallery.innerHTML = "";
+}
+
+function listenCategories() {
+    const buttons = document.querySelectorAll("#categories div");
+    for (const button of buttons) {
+        button.addEventListener("click", (e) => {
+            clickedButton = e.target;
+            clearGallery();
+            if (clickedButton.id == "0") {
+                displayImages(allWork);
+            }
+            if (clickedButton.id == "1") {
+                displayImages(objWork);
+            }
+            if (clickedButton.id == "2") {
+                displayImages(apptWork);
+            }
+            if (clickedButton.id == "3") {
+                displayImages(hotelWork);
+            }
+        });
+    }
+}
+
+setAdminElement();
+initiateWorksSet();
+listenCategories();
+
+//---- afficher preview de l'upload image sur la pop-up ----
 function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
 
         reader.onload = function (e) {
-            $("#blah").attr("src", e.target.result);
+            gid("preview").setAttribute("src", e.target.result);
         };
 
         reader.readAsDataURL(input.files[0]);
     }
 }
 
+//---- construction du form pour upload une image vers le serv ----
 const form = document.querySelector("#workform");
 let fileInput = gid("image");
 let titreInput = gid("titre");
 let categoryInput = gid("category");
 
+//---- fetch et envoi du formulaire au serv ----
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData();
