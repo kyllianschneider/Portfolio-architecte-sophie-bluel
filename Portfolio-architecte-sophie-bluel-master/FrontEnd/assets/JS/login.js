ad436366482c5af5ecab457f1d1f322d4eabbe9d
kidlysama <kylli444@gmail.com>
2023-03-13 19:15:45 +0100

    modified connection method

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/JS/login.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/JS/login.js
@@ -1,27 +1,28 @@
-
+const formLogin = document.querySelector("#form");
 //---- envoie de la connection et gérance des erreur ----
-gid("connection").addEventListener("click", function (e) {
-  fetch(serv + "api/users/login", {
-    mode: "cors",
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify({
-      email: gid("email").value,
-      password: gid("password").value,
-    }),
-  }).then((r) => {
-    if (r.status == 401) {
-      gid("error_zone").innerHTML = "Mot de passe incorrect";
-    } else if (r.status == 404) {
-      gid("error_zone").innerHTML = "Utilisateur incorrect";
-    } else {
-      r.json().then((data) => {
-        //deleteAllCookies();
-        setCookie("token", data.token, 1);
-        window.location.href = "./index.html";
-      });
-    }
+formLogin.addEventListener("submit", (e) => {
+  e.preventDefault();
+    fetch(serv + "api/users/login", {
+      mode: "cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({
+        email: gid("email").value,
+        password: gid("password").value,
+      }),
+    }).then((r) => {
+      if (r.status == 401) {
+        gid("error_zone").innerHTML = "Mot de passe incorrect";
+      } else if (r.status == 404) {
+        gid("error_zone").innerHTML = "Utilisateur incorrect";
+      } else {
+        r.json().then((data) => {
+          //deleteAllCookies();
+          setCookie("token", data.token, 1);
+          window.location.href = "./index.html";
+        });
+      }
+    });
   });
-});
